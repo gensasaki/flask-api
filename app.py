43c88b0164dde83f0c91423b93a6ff79982f62a5
Gen Sasaki <sasakigen11@gmail.com>
2017-10-23 00:13:24 -0700

    Add jsonify

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello world!"
+stores = [
+    {
+        'name': 'Special store',
+        'items': [
+            {
+                'name': 'My Item',
+                'price': 15.0
+            }
+        ]
+    }
+]
+
+@app.route('/', methods=['POST'])
+def create_store():
+    pass
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+@app.route('/store/')
+def get_stores():
+    return jsonify({'stores': stores})
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    pass
 
 app.run(port=5000)
